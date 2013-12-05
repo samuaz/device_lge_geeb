0192cb4ddf941e7ac6730565686c13fcaa90fbe4
samuaz <samuaz@gmail.com>
2013-12-04 20:35:46 -0600

    revert egl.cfg but disable mdp composition and set the composition and ui rendering to the cpu, and set art as default

--- device.mk
+++ device.mk
@@ -22,8 +22,8 @@
 #enable art and dalvik by default, and also make this shit odexed
 DISABLE_DEXPREOPT := false
 WITH_DEXPREOPT := true
-PRODUCT_RUNTIMES := runtime_libdvm_default
-PRODUCT_RUNTIMES += runtime_libart
+PRODUCT_RUNTIMES := runtime_libdvm
+PRODUCT_RUNTIMES += runtime_libart_default
 
 DEVICE_PACKAGE_OVERLAYS := device/lge/geeb/overlay
 
@@ -102,7 +102,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
-
 # NFC firmware
 PRODUCT_COPY_FILES += \
    device/lge/geeb/prebuilt/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
@@ -156,26 +155,33 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.call_ring.multiple=0
 
+#gpu tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.hwc.mdpcomp.enable=false \
+debug.sf.hw=1 \
+persist.sys.ui.hw=1 \
+debug.egl.hw=1 \
+debug.composition.type=gpu \
 debug.enable.wl_log=0 \
-debug.mdpcomp.logs=0
+persist.hwc.mdpcomp.enable=false \
+debug.mdpcomp.maxlayer=0 \
+debug.mdpcomp.logs=0 \
+debug.enabletr=true \
+windowsmgr.max_events_per_sec=180 \
+ro.hwui.texture_cache_size=72 \
+ro.hwui.layer_cache_size=48 \
+ro.hwui.r_buffer_cache_size=8 \
+ro.hwui.path_cache_size=32 \
+ro.hwui.gradient_cache_size=1 \
+ro.hwui.drop_shadow_cache_size=6 \
+ro.hwui.texture_cache_flushrate=0.4 \
+ro.hwui.text_small_cache_width=1024 \
+ro.hwui.text_small_cache_height=1024 \
+ro.hwui.text_large_cache_width=2048 \
+ro.hwui.text_large_cache_height=1024
 
+#dalvik tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
-
-PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.dexopt-flags=v=n,o=a,u=n,m=n \
+dalvik.vm.dexopt-flags=v=n,o=a,u=n \
 dalvik.vm.verify-bytecode=false
 
 PRODUCT_PACKAGES += \
@@ -255,8 +261,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	bdAddrLoader \
 	libwfcu \
-	conn_init \
-	OmniTorch
+	conn_init
 
 PRODUCT_PACKAGES += \
 	keystore.msm8960
@@ -279,7 +284,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
-	wifi.supplicant_scan_interval=15
+	wifi.supplicant_scan_interval=9
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
